34c71b01ba81d8f23ccd76e494bb3bc661d0677a
Xinlin Zhou <XZhou@lcs.on.ca>
2023-11-16 12:23:04 -0500

    Have put storage properties of a cone into the model.

--- GeometryHelper/Model/Cone.swift
+++ GeometryHelper/Model/Cone.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct Cone {
+    
+    // MARK: Stored properties
+    var radius1: Double
+    var slant: Double
+    var height: Double
+    
+    // MARK: Computed properties
+    var coneBaseArea: Double {
+        return 1 * Double.pi * radius1 * radius1
+    }
+    
+    var coneLateralSurfaceArea: Double {
+        return 1 * Double.pi * radius1 * slant
+    }
+    
+    var coneTotalArea: Double {
+        return 1 * Double.pi * radius1 * radius1 + 1 * Double.pi * radius1 * slant
+    }
+    
+    var coneVolume: Double {
+        return 1 / 3 * 1 * Double.pi * radius1 * radius1 * height
+    }
+}
